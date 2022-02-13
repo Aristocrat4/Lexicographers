04103af4e197c4ae50c77c9f82ca14b0348dfe52
aivazashvilitekla <tekla.aivazashvili@gau.edu.ge>
2022-02-13 22:52:53 +0400

    added contact page

--- src/js/script.js
+++ src/js/script.js
@@ -2,26 +2,30 @@
 const readMore = document.querySelector('.more-click');
 const text = document.querySelector('.text');
 
-readMore.addEventListener('click',(e)=>{
-    text.classList.toggle('show-more');
-    if(readMore.innerHTML==='წაიკითხეთ სრულად'){
-        readMore.innerHTML = 'შეამცირეთ ტექსტი';
-    }
-    else{
-        readMore.innerHTML = 'წაიკითხეთ სრულად';
-    }
-})
+if (readMore) {
+    readMore.addEventListener('click',(e)=>{
+        text.classList.toggle('show-more');
+        if(readMore.innerHTML==='წაიკითხეთ სრულად'){
+            readMore.innerHTML = 'შეამცირეთ ტექსტი';
+        }
+        else{
+            readMore.innerHTML = 'წაიკითხეთ სრულად';
+        }
+    })
+}
 
 const readMores = document.querySelector('.for-second-more');
 const text2 = document.querySelector('.second-text-txt');
 
-readMores.addEventListener('click',(e)=>{
-    text2.classList.toggle('show-more');
-    if(readMores.innerHTML==='წაიკითხეთ სრულად'){
-        readMores.innerHTML = 'შეამცირეთ ტექსტი';
-    }
-    else{
-        readMores.innerHTML = 'წაიკითხეთ სრულად';
-    }
-})
+if (readMores) {
+    readMores.addEventListener('click',(e)=>{
+        text2.classList.toggle('show-more');
+        if(readMores.innerHTML==='წაიკითხეთ სრულად'){
+            readMores.innerHTML = 'შეამცირეთ ტექსტი';
+        }
+        else{
+            readMores.innerHTML = 'წაიკითხეთ სრულად';
+        }
+    })
+}
 // end of show more text function
