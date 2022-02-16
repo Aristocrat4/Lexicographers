1d8963dee4091bd4d882622d3025738676a6f411
aivazashvilitekla <tekla.aivazashvili@gau.edu.ge>
2022-02-16 21:14:30 +0400

    new term page

--- src/js/script.js
+++ src/js/script.js
@@ -33,19 +33,38 @@ if (readMores) {
 // Form Validation
 
 const sendButton = document.getElementById('sendButton');
-sendButton.addEventListener('click', () => {
-    const nameField = document.getElementById('name');
-    const emailField = document.getElementById('email');
-    const messageField = document.getElementById('message');
-    const emptyValue = [nameField, emailField, messageField].filter(item => !item.value);
-    const filledValue = [nameField, emailField, messageField].filter(item => item.value);
-    emptyValue.forEach(item => {
-        item.classList.add('errorBorder');
-        item.nextSibling.nextSibling.classList.remove('hidden');
-    });
-    filledValue.forEach(item => {
-        item.classList.remove('errorBorder');
-        item.nextSibling.nextSibling.classList.add('hidden');
-    });
-})
+if (sendButton) {
+    sendButton.addEventListener('click', () => {
+        const nameField = document.getElementById('name');
+        const emailField = document.getElementById('email');
+        const messageField = document.getElementById('message');
+        const emptyValue = [nameField, emailField, messageField].filter(item => !item.value);
+        const filledValue = [nameField, emailField, messageField].filter(item => item.value);
+        emptyValue.forEach(item => {
+            item.classList.add('errorBorder');
+            item.nextSibling.nextSibling.classList.remove('hidden');
+        });
+        filledValue.forEach(item => {
+            item.classList.remove('errorBorder');
+            item.nextSibling.nextSibling.classList.add('hidden');
+        });
+    })
+}
+
 // End of Form Validation
+
+// new term script
+// const newTermInput = document.getElementById('new-term');
+// const btnAddNewTerm = document.getElementById('addNewTerm');
+// if (newTermInput) {
+//     newTermInput.addEventListener('change', (event) => {
+//         console.log(event.target.value);
+//         if (newTermInput.value) {
+//             btnAddNewTerm.disabled = false;
+//         } else {
+//             btnAddNewTerm.disabled = true;
+//         }
+        
+//     });
+// }
+// end of new term script
