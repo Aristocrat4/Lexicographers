198f59644efa80001da43f7054b1c26b699c3636
Aristocrat4 <galadanelia1@gmail.com>
2022-02-20 20:45:48 +0400

    add validations for login and signup forms and hover effects on their buttons

--- src/js/script.js
+++ src/js/script.js
@@ -56,17 +56,16 @@ if (sendButton) {
 // End of Form Validation
 
 // new term script
-const newTermInput = document.getElementById('new-term');
-const btnAddNewTerm = document.getElementById('addNewTerm');
+const newTermInput = document.getElementById("new-term");
+const btnAddNewTerm = document.getElementById("addNewTerm");
 if (newTermInput) {
-    newTermInput.addEventListener('input', (event) => {
-        if (newTermInput.value) {
-            btnAddNewTerm.disabled = false;
-        } else {
-            btnAddNewTerm.disabled = true;
-        }
-
-    });
+  newTermInput.addEventListener("input", (event) => {
+    if (newTermInput.value) {
+      btnAddNewTerm.disabled = false;
+    } else {
+      btnAddNewTerm.disabled = true;
+    }
+  });
 }
 // end of new term script
 
@@ -94,9 +93,8 @@ const signUpBtn = document.querySelector(".btn-signup");
 const signInForm = document.querySelector(".login");
 const signUpForm = document.querySelector(".signup");
 // click on buttons
-if (signUpBtn){
+if (signUpBtn) {
   signUpBtn.addEventListener("click", () => {
-    console.log(signUpBtn);
     signUpBtn.style.background = "transparent";
     signInBtn.style.background = "#798996";
     signUpBtn.style.color = "#798996";
@@ -107,7 +105,6 @@ if (signUpBtn){
 }
 if (signInBtn) {
   signInBtn.addEventListener("click", () => {
-    console.log(signUpBtn);
     signInBtn.style.background = "transparent";
     signUpBtn.style.background = "#798996";
     signInBtn.style.color = "#798996";
@@ -119,9 +116,8 @@ if (signInBtn) {
 
 // -------- end of toggle login and signup forms -------
 
-
 // -------- change autorisation form to registration form --------
-function changeAutoToRegistr(){
+function changeAutoToRegistr() {
   signUpBtn.style.background = "transparent";
   signInBtn.style.background = "#798996";
   signUpBtn.style.color = "#798996";
@@ -131,3 +127,59 @@ function changeAutoToRegistr(){
 }
 // -------- end of function --------
 
+// -------- Login Validation -------
+const loginBtn = document.querySelector(".submit-login");
+if (loginBtn) {
+  loginBtn.addEventListener("click", () => {
+    const usernameInput = document.querySelector(".username");
+    const passwordInput = document.querySelector(".psw");
+    const emptyInput = [usernameInput, passwordInput].filter(
+      (item) => !item.value
+    );
+    const filledInput = [usernameInput, passwordInput].filter(
+      (item) => item.value
+    );
+    emptyInput.forEach((item) => {
+      item.classList.add("errorBorder");
+      item.nextSibling.nextSibling.classList.remove("hidden");
+    });
+    filledInput.forEach((item) => {
+      item.classList.remove("errorBorder");
+      item.nextSibling.nextSibling.classList.add("hidden");
+    });
+  });
+}
+// --------- Registration Validation ------------
+const registrationBtn = document.querySelector(".submit-registration");
+if (registrationBtn) {
+  registrationBtn.addEventListener("click", () => {
+    const emailRegInput = document.querySelector(".email-reg");
+    const nameRegInput = document.querySelector(".name-reg");
+    const phoneRegInput = document.querySelector(".phone-reg");
+    const pswRegInput = document.querySelector(".psw-reg");
+    const pswRepeatRegInput = document.querySelector(".psw-repeat-reg");
+    const emptyRegInputs = [
+      emailRegInput,
+      nameRegInput,
+      phoneRegInput,
+      pswRegInput,
+      pswRepeatRegInput,
+    ].filter((item) => !item.value);
+    const FilledRegInputs = [
+      emailRegInput,
+      nameRegInput,
+      phoneRegInput,
+      pswRegInput,
+      pswRepeatRegInput,
+    ].filter((item) => item.value);
+
+    emptyRegInputs.forEach((item) => {
+      item.classList.add("errorBorder");
+      item.nextSibling.nextSibling.classList.remove("hidden");
+    });
+    FilledRegInputs.forEach((item) => {
+      item.classList.remove("errorBorder");
+      item.nextSibling.nextSibling.classList.add("hidden");
+    });
+  });
+}
